a533cc01cb96d9a958ef6c0f9677a4ea6287d4cd
Dinesh <104751442+Ramos679@users.noreply.github.com>
2026-04-19 15:52:44 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content:[
     "./pages/**/*.{js,jsx,ts,tsx}",
@@ -6,4 +7,5 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins:
+  plugins:[],
+}
